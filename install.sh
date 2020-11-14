3ed1dada77eecce13a668cfeaabcac4da4424d6b
rr-boyz <74429998+rr-boyz@users.noreply.github.com>
2020-11-14 17:04:34 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+apt update
+
+apt install python -y
+apt install python2 -y
+apt install ruby -y
+apt install figlet -y
+gem install lolcat
+echo '
+ ==============================================
+|         Now Type python3 ipinfo.py           |
+ =============================================='
